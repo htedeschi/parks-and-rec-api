a3d9e7e6d9273e11cfb0478fdc04e8d14510c356
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-05-31 17:48:26 -0600

    Added recaptcha server-side

--- routes/accounts.js
+++ routes/accounts.js
@@ -93,65 +93,104 @@ router.post("/", (req, res) => {
 		return;
 	}
 
-	const email = req.body.email;
-	const token = jwt.sign({ email }, process.env.JWT_KEY);
+	if (!req.body["g-recaptcha-response"]) {
+		res.status(422);
+		res.statusMessage = 'Missing parameter "g-recaptcha-response"';
+		res.end();
+		return;
+	}
 
-	const account = new Account({
-		email,
-		token,
-	});
+	const https = require("https");
 
-	account
-		.save()
-		.then((data) => {
-			console.log(`Success! ${data}`);
+	const recaptcha_secret = process.env.GRECAPTCHA;
+	const recaptcha_response = req.body["g-recaptcha-response"];
+	const recaptcha_url = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptcha_secret}&response=${recaptcha_response}`;
 
-			const address =
-				req.protocol +
-				"://" +
-				req.hostname +
-				(req.app.settings.port ? ":" + req.app.settings.port : "") +
-				"/accounts/confirm/" +
-				data._id;
+	https.get(recaptcha_url, (resp) => {
+		let data = "";
 
-			// send email to confirm email
-			nodemailer.createTestAccount((err, account) => {
-				// setup email data with unicode symbols
-				let mailOptions = {
-					from: '"Parks and Rec API" <api@henriquetedeschi.com>', // sender address
-					replyTo: `"$Parks and Rec API" <api@henriquetedeschi.com>`,
-					to: `"${email}" <${email}>`, // list of receivers
-					subject: "Confirm your email - Parks and Rec API", // Subject line
-					html: `Hello!
+		// A chunk of data has been recieved.
+		resp.on("data", (chunk) => {
+			data += chunk;
+		});
+
+		resp.on("end", () => {
+			data = JSON.parse(data);
+			// console.log(data)
+			// return;
+
+			if (!data.success) {
+				res.status(401);
+				res.statusMessage =
+					"Google reCAPTCHA considered you a robot, try again";
+				res.end();
+				return;
+			}
+
+			const email = req.body.email;
+			const token = jwt.sign({ email }, process.env.JWT_KEY);
+
+			const account = new Account({
+				email,
+				token,
+			});
+
+			account
+				.save()
+				.then((data) => {
+					console.log(`Success! ${data}`);
+
+					const address =
+						req.protocol +
+						"://" +
+						req.hostname +
+						(req.app.settings.port
+							? ":" + req.app.settings.port
+							: "") +
+						"/accounts/confirm/" +
+						data._id;
+
+					// send email to confirm email
+					nodemailer.createTestAccount((err, account) => {
+						// setup email data with unicode symbols
+						let mailOptions = {
+							from:
+								'"Parks and Rec API" <api@henriquetedeschi.com>', // sender address
+							replyTo: `"$Parks and Rec API" <api@henriquetedeschi.com>`,
+							to: `"${email}" <${email}>`, // list of receivers
+							subject: "Confirm your email - Parks and Rec API", // Subject line
+							html: `Hello!
                         <br>
                         Before continuing with your token, please confirm your email clicking <a href="${address}">here</a> or copying and pasting the following text to your browser address bar:
                         <br>
                         <span>${address}</span>`, // html body
-				};
+						};
 
-				// send mail with defined transport object
-				transporter.sendMail(mailOptions, (error, info) => {
-					if (error) {
-						res.json({ success: false });
-						res.end();
-						return console.log(error);
-					}
-					// console.log("Message sent: %s", info.messageId);
-					// Preview only available when sending through an zoho account
-					// console.log(
-					// 	"Preview URL: %s",
-					// 	nodemailer.getTestMessageUrl(info)
-					// );
+						// send mail with defined transport object
+						transporter.sendMail(mailOptions, (error, info) => {
+							if (error) {
+								res.json({ success: false });
+								res.end();
+								return console.log(error);
+							}
+							// console.log("Message sent: %s", info.messageId);
+							// Preview only available when sending through an zoho account
+							// console.log(
+							// 	"Preview URL: %s",
+							// 	nodemailer.getTestMessageUrl(info)
+							// );
 
-					res.json({ success: true, date: new Date() });
-					res.end();
+							res.json({ success: true, date: new Date() });
+							res.end();
+						});
+					});
+				})
+				.catch((err) => {
+					console.log(`Error! ${err}`);
+					res.json(err);
 				});
-			});
-		})
-		.catch((err) => {
-			console.log(`Error! ${err}`);
-			res.json(err);
 		});
+	});
 });
 
 module.exports = router;
